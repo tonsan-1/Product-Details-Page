02baeae8acc35cc695b6019ca11a01718a6ff1ab
Stoyan Kostadinov <tonsan221@gmail.com>
2021-11-04 08:02:58 +0200

    adding shoes object and shoes images

---
+++ src/assets/js/shoes.js
@@ -0,0 +1,26 @@
+export const shoes = {
+    "green": {
+        "id": "812d8359-c545-4b66-8355-16d1a9e2cb4a",
+        "name": "adidas Hoops 2.0 Mid",
+        "images": ["./assets/img/hoops/1.png", "./assets/img/hoops/2.png", "./assets/img/hoops/3.png"],
+        "price": 80.00,
+        "stock": "In stock",
+        "sizes": [41,42,45]
+    },
+    "black": {
+        "id": "800ec515-ad2d-41dd-8f9a-80906c870adb",
+        "name": "adidas Terrex AX3",
+        "images": ["./assets/img/terrex/1.png", "./assets/img/terrex/2.png", "./assets/img/terrex/3.png"],
+        "price": 110.00,
+        "stock": "Out of stock",
+        "sizes": []
+    },
+    "beige": {
+        "id": "80d621b7-4151-4042-b11d-1badee1962af",
+        "name": "Nike Manoa Leather",
+        "images": ["./assets/img/manoa/1.png", "./assets/img/manoa/2.png", "./assets/img/manoa/3.png"],
+        "price": 145.00,
+        "stock": "Nearly gone",
+        "sizes": [40,41]
+    }
+}
